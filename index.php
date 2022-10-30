19fb006bd7173719bcdedfe820738d48166987b0
Raihanah Qonita <113325157+raihanahqonita@users.noreply.github.com>
2022-10-30 21:36:38 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+ echo "Hello World!<br>";
+
+ $nama = "Raihanah";
+ $umur = 20;
+
+ echo " Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+ $namaAdik = "Nah";
+ $umurAdik = 15;
+
+ echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+ $selisihUmur = $umur-$umurAdik;
+ echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun.";
+
+?>
